0d568d6ffb0c75150026559994262ef0198d4882
hongson2410 <phamhongson2410@gmail.com>
2020-11-30 22:34:51 +0700

    up

---
+++ module2/src/bai1_introduction_to_java/thuc_hanh/systemtime.java
@@ -0,0 +1,9 @@
+package bai1_introduction_to_java.thuc_hanh;
+import java.util.Date;
+public class systemtime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
