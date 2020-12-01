ff1ed1f4696c976e77dc0d7c8b3763be6f0ca708
hongson2410 <phamhongson2410@gmail.com>
2020-12-01 23:41:07 +0700

    up

--- module2/src/bai2_array_loop/bai_tap/HienThiCacLoaiHinh.java
+++ module2/src/bai2_array_loop/bai_tap/HienThiCacLoaiHinh.java
@@ -1,4 +1,88 @@
 package bai2_array_loop.bai_tap;
 
+import java.util.Scanner;
+
 public class HienThiCacLoaiHinh {
+    public static void main(String[] args) {
+
+        int choice;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle");
+            System.out.println("3. Print isosceles triangle");
+            System.out.println("4. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the width: ");
+                    int width = input.nextInt();
+                    System.out.println("Enter the height: ");
+                    int height = input.nextInt();
+                    for (int i = 0; i < height; i++) {
+                        for (int j = 0; j < width; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter the length: ");
+                    int length = input.nextInt();
+                    for (int i = 1; i <= length; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    for (int i = length; i >= 1; i--) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    for (int i = 1; i <= length; i++) {
+                        for (int j = 0; j < length - i; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    for (int i = length; i >= 1; i--) {
+                        for (int j = 0; j < length - i; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter the height: ");
+                    int a = input.nextInt();
+                    for (int i = 1; i <= a; i++) {
+                        for (int j =0 ; j< a-i; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int j = 0; j < i; j++) {
+                            System.out.print(" *");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+        }while (choice !=4);
+    }
 }
---
+++ module2/src/bai2_array_loop/bai_tap/SoNguyenTo20.java
@@ -0,0 +1,24 @@
+package bai2_array_loop.bai_tap;
+
+public class SoNguyenTo20 {
+    public static void main(String[] args) {
+        int count =0;
+        int number =2;
+        while (count<20){
+            int i =2;
+            boolean check= true;
+            while (i<= Math.sqrt(number)){
+                if (number % i ==0){
+                    check=false;
+                    break;
+                }
+                i++;
+            }
+            if (check){
+                System.out.println(number);
+                count++;
+            }
+            number++;
+        }
+    }
+}
