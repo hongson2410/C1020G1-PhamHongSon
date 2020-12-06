4678d4bb6e2f9f05ae68cc7829d6181c34f26cef
hongson2410 <phamhongson2410@gmail.com>
2020-12-06 14:49:15 +0700

    up

--- module2/src/bai5_access_modifier/thuc_hanh/access_modifier/Default/pack/A.java
+++
@@ -1,8 +0,0 @@
-package bai5_access_modifier.thuc_hanh.access_modifier.Default;
-
-class A {
-    private int data=40;
-
-    private void msg(){System.out.println("Hello java");}
-
-}
--- module2/src/bai5_access_modifier/thuc_hanh/access_modifier/Private/A.java
+++
@@ -1,5 +0,0 @@
-package bai5_access_modifier.thuc_hanh.access_modifier.Private;
-
-class A {
-    void msg(){System.out.println("Hello");}
-}
