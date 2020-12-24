42e0d0a12740664dadf8e8ba756bf98842509857
hongson2410 <phamhongson2410@gmail.com>
2020-12-24 16:05:11 +0700

    up

--- module2/src/bai18_string_regex/bai_tap/number_phone/NumberPhoneRegexTest.java
+++ module2/src/bai18_string_regex/bai_tap/number_phone/NumberPhoneRegexTest.java
@@ -2,12 +2,12 @@ package bai18_string_regex.bai_tap.number_phone;
 
 public class NumberPhoneRegexTest {
     private static NumberPhoneRegex numberPhoneRegex;
-    private static String[] phoneNumber={"03-0135968452", "a5-563498","88-0569435687","000968561457","06-2365978451","880945368751"};
+    private static String[] phoneNumber = {"03-0135968452", "a5-563498", "88-0569435687", "000968561457", "06-2365978451", "880945368751"};
 
     public static void main(String[] args) {
-        numberPhoneRegex=new NumberPhoneRegex();
-        for (String phone: phoneNumber){
-            System.out.println("Number phone is: "+ phone+ " is valid "+ numberPhoneRegex.validate(phone) );
+        numberPhoneRegex = new NumberPhoneRegex();
+        for (String phone : phoneNumber) {
+            System.out.println("Number phone is: " + phone + " is valid " + numberPhoneRegex.validate(phone));
         }
     }
 }
