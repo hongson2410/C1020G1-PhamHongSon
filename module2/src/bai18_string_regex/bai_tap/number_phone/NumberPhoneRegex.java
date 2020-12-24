7beb97833077924708e0e1ee92a5288c5bb7b7ef
hongson2410 <phamhongson2410@gmail.com>
2020-12-24 16:27:23 +0700

    up

--- module2/src/bai18_string_regex/bai_tap/number_phone/NumberPhoneRegex.java
+++ module2/src/bai18_string_regex/bai_tap/number_phone/NumberPhoneRegex.java
@@ -1,7 +1,7 @@
 package bai18_string_regex.bai_tap.number_phone;
 
 public class NumberPhoneRegex {
-    private static final String NUMBER_PHONE_REGEX = "[0-9]{2}[-][0][0-9]{9}";
+    private static final String NUMBER_PHONE_REGEX = "\\d{2}[-][0]\\d{9}";
 
     public boolean validate(String regex) {
         return regex.matches(NUMBER_PHONE_REGEX);
