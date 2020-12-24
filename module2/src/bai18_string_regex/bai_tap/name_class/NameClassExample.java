0555c37315576e88bed0ab32891dd5a0eea51084
hongson2410 <phamhongson2410@gmail.com>
2020-12-24 16:34:52 +0700

    up

--- module2/src/bai18_string_regex/bai_tap/name_class/NameClassExample.java
+++ module2/src/bai18_string_regex/bai_tap/name_class/NameClassExample.java
@@ -1,7 +1,7 @@
 package bai18_string_regex.bai_tap.name_class;
 
 public class NameClassExample {
-    private static final String CLASS_NAME_REGEX = "[CAP][0-9]{4}[GHIKLM]";
+    private static final String CLASS_NAME_REGEX = "[CAP]\\d{4}[GHIKLM]";
 
     public boolean validate(String regex) {
         return regex.matches(CLASS_NAME_REGEX);
